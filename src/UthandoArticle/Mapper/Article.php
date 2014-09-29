c8d9375d3b3990edc4bb63556ed158b9bc2141e9
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-09-29 21:18:37 +0100

    update module config

--- src/UthandoArticle/Mapper/Article.php
+++ src/UthandoArticle/Mapper/Article.php
@@ -2,20 +2,18 @@
 
 namespace UthandoArticle\Mapper;
 
-use UthandoCommon\Mapper\AbstractMapper;
+use UthandoCommon\Mapper\AbstractDbMapper;
 
-class Article extends AbstractMapper
+class Article extends AbstractDbMapper
 {
 	protected $table = 'article';
 	protected $primary = 'articleId';
-	protected $model = 'UthandoArticle\Model\Article';
-	protected $hydrator = 'UthandoArticle\Hydrator\Article';
 	
 	public function getArticleBySlug($slug)
 	{
 		$select = $this->getSelect()->where(['slug' => $slug]);
-		$rowset = $this->fetchResult($select);
-		$row = $rowset->current();
+		$rowSet = $this->fetchResult($select);
+		$row = $rowSet->current();
 		return $row;
 	}
 }
