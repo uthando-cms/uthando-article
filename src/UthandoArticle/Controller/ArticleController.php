b0b9dcda5334420e1250b60be4c0344a36269406
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-11-06 05:56:08 +0000

    fixed exception when page is not found

--- src/UthandoArticle/Controller/ArticleController.php
+++ src/UthandoArticle/Controller/ArticleController.php
@@ -1,42 +1,55 @@
 <?php
+/**
+ * Uthando CMS (http://www.shaunfreeman.co.uk/)
+ *
+ * @package   UthandoArticle\Controller
+ * @author    Shaun Freeman <shaun@shaunfreeman.co.uk>
+ * @copyright Copyright (c) 2014 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license   see LICENSE
+ */
 
 namespace UthandoArticle\Controller;
 
 use UthandoCommon\Controller\AbstractCrudController;
 use Zend\View\Model\ViewModel;
 
+/**
+ * Class ArticleController
+ *
+ * @package UthandoArticle\Controller
+ */
 class ArticleController extends AbstractCrudController
 {
     protected $controllerSearchOverrides = array('sort' => 'articleId');
     protected $serviceName = 'UthandoArticle';
     protected $route = 'admin/article';
-    
+
     public function viewAction()
     {
         $viewModel = new ViewModel();
-        
+
         if ($this->getRequest()->isXmlHttpRequest()) {
             $viewModel->setTerminal(true);
         }
-        
+
         $slug = $this->params()->fromRoute('slug');
         $page = $this->getService()->getArticleBySlug($slug);
 
-        if (!$this->isAllowed($page->getResource())) {
-            throw new \Exception('Not allowed!');
-        }
-        
         if (!$page) {
             $viewModel->setTemplate('uthando-article/article/404');
             return $viewModel;
         }
-        
+
+        if (!$this->isAllowed($page->getResource())) {
+            throw new \Exception('Not allowed!');
+        }
+
         $this->getService()->addPageHit($page);
-        
+
         if ($this->params('model') == true) {
             $viewModel->setTemplate('uthando-article/article/model');
         }
-        
+
         return $viewModel->setVariables(['page' => $page]);
     }
 }
--- src/UthandoArticle/Service/Article.php
+++ src/UthandoArticle/Service/Article.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Uthando CMS (http://www.shaunfreeman.co.uk/)
+ *
+ * @package   UthandoArticle\Service
+ * @author    Shaun Freeman <shaun@shaunfreeman.co.uk>
+ * @copyright Copyright (c) 2014 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license   see LICENSE
+ */
 
 namespace UthandoArticle\Service;
 
@@ -7,25 +15,30 @@ use UthandoCommon\Model\ModelInterface;
 use UthandoCommon\Service\AbstractRelationalMapperService;
 use Zend\Form\Form;
 
+/**
+ * Class Article
+ *
+ * @package UthandoArticle\Service
+ */
 class Article extends AbstractRelationalMapperService
-{	
-	/**
-	 * @var \UthandoNavigation\Service\MenuItem
-	 */
-	protected $menuItemService;
+{
+    /**
+     * @var \UthandoNavigation\Service\MenuItem
+     */
+    protected $menuItemService;
 
     /**
      * @var string
      */
-	protected $serviceAlias = 'UthandoArticle';
+    protected $serviceAlias = 'UthandoArticle';
 
     /**
      * @var array
      */
     protected $referenceMap = [
-        'user'  => [
-            'refCol'    => 'userId',
-            'service'   => 'UthandoUser',
+        'user' => [
+            'refCol' => 'userId',
+            'service' => 'UthandoUser',
         ],
     ];
 
@@ -41,42 +54,44 @@ class Article extends AbstractRelationalMapperService
 
         return $article;
     }
-	
-	public function getArticleBySlug($slug)
-	{
-		$slug = (string) $slug;
+
+    public function getArticleBySlug($slug)
+    {
+        $slug = (string)$slug;
         $article = $this->getMapper()->getArticleBySlug($slug);
 
-        $this->populate($article, true);
+        if ($article) {
+            $this->populate($article, true);
+        }
 
         return $article;
-	}
-	
-	public function addPageHit(ArticleModel $article)
-	{
-		$pageHits = $article->getPageHits();
-		$pageHits++;
-		$article->setPageHits($pageHits);
-		$this->save($article);
-	}
-	
-	public function add(array $post, Form $form = null)
-	{
-		if (!$post['slug']) {
-			$post['slug'] = $post['title'];
-		}
-		
-		$insertId = parent::add($post);
-		
-		if (!$insertId instanceof Form) {
-    		$pageResult = $this->updateMenuItem(
-    		    $this->getById($insertId),
-    		    $post
-    		);
-    	}
-		
-		return $insertId;
-	}
+    }
+
+    public function addPageHit(ArticleModel $article)
+    {
+        $pageHits = $article->getPageHits();
+        $pageHits++;
+        $article->setPageHits($pageHits);
+        $this->save($article);
+    }
+
+    public function add(array $post, Form $form = null)
+    {
+        if (!$post['slug']) {
+            $post['slug'] = $post['title'];
+        }
+
+        $insertId = parent::add($post);
+
+        if (!$insertId instanceof Form) {
+            $pageResult = $this->updateMenuItem(
+                $this->getById($insertId),
+                $post
+            );
+        }
+
+        return $insertId;
+    }
 
     /**
      * @param ModelInterface $article
@@ -84,44 +99,44 @@ class Article extends AbstractRelationalMapperService
      * @param Form $form
      * @return int
      */
-	public function edit(ModelInterface $article, array $post, Form $form = null)
-	{
-		$article->setDateModified();
-		$result = parent::edit($article, $post);
-		
-		// find page first, if exists delete it before updating.
-		
-		if (!$result instanceof Form) {
-		    $pageResult = $this->updateMenuItem(
-		        $this->getById($article->getArticleId()), $post
-	        );
-		}
-		
-		return $result;
-	}
-	
-	/**
-	 * @param int $limit
-	 * @return Ambigous <\Zend\Db\ResultSet\HydratingResultSet, \Zend\Db\ResultSet\ResultSet, \Zend\Paginator\Paginator>
-	 */
-	public function getRecentPosts($limit)
-	{
-	    $limit = (int) $limit;
-	    /* @var $mapper \UthandoArticle\Mapper\Article */
-	    $mapper = $this->getMapper();
-	    return $mapper->getArticlesByDate($limit);
-	}
+    public function edit(ModelInterface $article, array $post, Form $form = null)
+    {
+        $article->setDateModified();
+        $result = parent::edit($article, $post);
+
+        // find page first, if exists delete it before updating.
+
+        if (!$result instanceof Form) {
+            $pageResult = $this->updateMenuItem(
+                $this->getById($article->getArticleId()), $post
+            );
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param int $limit
+     * @return Ambigous <\Zend\Db\ResultSet\HydratingResultSet, \Zend\Db\ResultSet\ResultSet, \Zend\Paginator\Paginator>
+     */
+    public function getRecentPosts($limit)
+    {
+        $limit = (int)$limit;
+        /* @var $mapper \UthandoArticle\Mapper\Article */
+        $mapper = $this->getMapper();
+        return $mapper->getArticlesByDate($limit);
+    }
 
     /**
      * @param $article
      * @param $post
      * @throws \Exception
      */
-	protected function updateMenuItem($article, $post)
-	{
-	    if ($post['position'] && $post['menuInsertType'] != 'noInsert') {
-	        $ids = explode('-', $post['position']);
-	        $data = [
+    protected function updateMenuItem($article, $post)
+    {
+        if ($post['position'] && $post['menuInsertType'] != 'noInsert') {
+            $ids = explode('-', $post['position']);
+            $data = [
                 'menuId' => $ids[0],
                 'label' => $article->getTitle(),
                 'position' => $ids[1],
@@ -130,28 +145,28 @@ class Article extends AbstractRelationalMapperService
                 'resource' => '',
                 'visible' => 1,
                 'menuInsertType' => $post['menuInsertType']
-	        ];
-	         
-	        $page = $this->getMenuItemService()->getMenuItemByMenuIdAndLabel($ids[0], $article->getTitle());
-	
-	        if (!$page) {
-	            $this->getMenuItemService()->add($data);
-	        } else {
-	            $this->getMenuItemService()->edit($page, $data);
-	        }
-	    }
-	}
-	
-	/**
-	 * @return \UthandoNavigation\Service\MenuItem
-	 */
-	protected function getMenuItemService()
-	{
-	    if (!$this->menuItemService) {
-			$sl = $this->getServiceLocator();
-			$this->menuItemService = $sl->get('UthandoNavigationMenuItem');
-		}
-		
-		return $this->menuItemService;
-	}
+            ];
+
+            $page = $this->getMenuItemService()->getMenuItemByMenuIdAndLabel($ids[0], $article->getTitle());
+
+            if (!$page) {
+                $this->getMenuItemService()->add($data);
+            } else {
+                $this->getMenuItemService()->edit($page, $data);
+            }
+        }
+    }
+
+    /**
+     * @return \UthandoNavigation\Service\MenuItem
+     */
+    protected function getMenuItemService()
+    {
+        if (!$this->menuItemService) {
+            $sl = $this->getServiceLocator();
+            $this->menuItemService = $sl->get('UthandoNavigationMenuItem');
+        }
+
+        return $this->menuItemService;
+    }
 }
